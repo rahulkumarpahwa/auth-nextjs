c683222d35cd0e0859c2dc9b49db7fb037f4650e
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-05-16 23:18:05 +0530

    created signup backend route

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,14 +1,28 @@
 import nodemailer from "nodemailer";
+import User from "@/models/userModel";
+import bcryptjs from "bcryptjs";
 
 export const sendEmail = async ({ email, emailType, userId }: any) => {
   try {
+    const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+    if (emailType === "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 3600000,
+      });
+    } else if (emailType === "RESET") {
+      await User.findByIdAndUpdate(userId, {
+        forgetPasswordToken: hashedToken,
+        forgetPasswordTokenExpiry: Date.now() + 3600000,
+      });
+    }
+
     const transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false, // Use `true` for port 465, `false` for all other ports
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
       auth: {
-        user: "maddison53@ethereal.email",
-        pass: "jn7jnAPss4f63QBp6D",
+        user: "dcdb3d38f9d838", //should be in env
+        pass: "97a009119a9134", //should be in env
       },
     });
 
@@ -18,7 +32,7 @@ export const sendEmail = async ({ email, emailType, userId }: any) => {
       subject:
         emailType === "VERIFY" ? "Verify your email" : "Reset your password", // Subject line
 
-      html: "<b>Hello world?</b>", // html body
+      html: `<p>Hello world?</p>`, // html body
     };
 
     const response = await transporter.sendMail(mailOptions);
